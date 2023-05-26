362e5b1e4918595042932b27028e6f5b01c95444
kaniadhea <kaniadhea7@gmail.com>
2023-05-26 20:36:59 +0700

    catch

--- Double_LInked_List_170/Double_LInked_List_170.cpp
+++ Double_LInked_List_170/Double_LInked_List_170.cpp
@@ -153,6 +153,7 @@ void DoubleLinkedList::hapus() {
 void DoubleLinkedList::searchData() {
 	if (listEmpty() == true) {
 		cout << "\nList is empty" << endl;
+
 	}
 	Node* prev, * curr;
 	prev = curr = NULL;
@@ -171,38 +172,42 @@ void DoubleLinkedList::searchData() {
 int main() {
 	DoubleLinkedList obj;
 	while (true) {
-	}
-	try {
-		cout << "\nMenu" << endl;
-		cout << "1. Add a record to the list" << endl;
-		cout << "2. Delete a record from the list" << endl;
-		cout << "3. View all records in the ascending order of roll numbers" << endl;
-		cout << "4. View all records in the descending order of roll numbers" << endl;
-		cout << "5. Search for a record in the list" << endl;
-		cout << "6. Exit" << endl;
-		cout << "\nEnter your choice (1-6): ";
-		char ch;
-		cin >> ch;
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all records in the ascending order of roll numbers" << endl;
+			cout << "4. View all records in the descending order of roll numbers" << endl;
+			cout << "5. Search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your choice (1-6): ";
+			char ch;
+			cin >> ch;
 
-		switch (ch) {
-		case '1':
-			obj.addNode();
-			break;
-		case '2':
-			obj.hapus();
-		case '3':
-			obj.traverse();
-		case'4':
-			obj.revtraverse();
-		case '5':
-			obj.searchData();
-			break;
-		case'6':
-			return 0;
-		default:
-			cout << "\nInvalid option" << endl;
-			break;
-		}
+			switch (ch) {
+			case '1':
+				obj.addNode();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.traverse();
+				break;
+			case'4':
+				obj.revtraverse();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case'6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+			};
+
+		};
+		catch(exception& e)
 	};
-
-
+		
