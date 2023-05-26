fc8d31e7ae700efde673e636712d8946909168fb
kaniadhea <kaniadhea7@gmail.com>
2023-05-26 18:23:56 +0700

    class Node

---
+++ Double_LInked_List_170/Double_LInked_List_170.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+	int noMhs;
+	string name;
+	Node* next;
+	Node* prev;
+};
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node** privous, Node** current);
+	bool DeleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
+};
